f9c2bf580d4b5e306399c9e9f6e295ab4ef75153
Bogdan Sutuzhko <julfy-bs@yandex.ru>
2023-02-01 15:16:57 +0300

    feat: добавляет webpack.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './pages/index.css';
+
+const app = document.querySelector('.page');
+const canvas = document.createElement('canvas');
+canvas.classList.add('page__canvas');
+app.prepend(canvas);
+
